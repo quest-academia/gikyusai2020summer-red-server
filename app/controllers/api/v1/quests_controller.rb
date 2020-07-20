013c79076c76475882361232d75e1688941b1031
Hikoyuki <hikoyuki.sakai@gmail.com>
2020-07-20 22:51:52 +0900

    修正

--- app/controllers/api/v1/quests_controller.rb
+++ app/controllers/api/v1/quests_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::QuestsController < ApplicationController
   def show
     quest = Quest.find(params[:id])
-    render json: { data: quest, status: "alive" }
+    render json: { data: quest, success: true }
   end
 end
