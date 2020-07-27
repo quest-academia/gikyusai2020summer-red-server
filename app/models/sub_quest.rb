d04d4a865034aa6ee283d63227a2e5f7a964a05a
marimonon <mappi-@hotmail.co.jp>
2020-07-27 09:07:01 +0900

    source:の後ろのモデル名を単数形に変更

--- app/models/sub_quest.rb
+++ app/models/sub_quest.rb
@@ -2,5 +2,5 @@ class SubQuest < ApplicationRecord
   belongs_to :quest
   validates :title, presence: true
   has_many :user_sub_quests, dependent: :destroy
-  has_many :completed_users, through: :user_sub_quests, source: :users
+  has_many :completed_users, through: :user_sub_quests, source: :user
 end
