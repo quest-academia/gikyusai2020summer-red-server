944cc06f0a1cc735962972122c0e9ad3e34f2044
Hikoyuki <hikoyuki.sakai@gmail.com>
2020-08-04 22:49:04 +0900

    Quest更新エンドポイント

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'health_check', to: "health_check#show"
-      resources :quests, only: [:show, :destroy]
+      resources :quests, only: [:show, :destroy, :update]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
